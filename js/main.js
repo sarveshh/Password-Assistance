f8c54cf49b5c6c9c8f9dbf14602ce9adfec83cf1
Sarvesh Patil <sarveshp273@gmail.com>
2020-09-08 21:30:13 +0530

    Initial setup

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// declare varaibles from dom after load to make sure they are current 
+
+const results = document.querySelector("#result");
+const UNInum =[48,57];
+const UNIupper = [65,90];
+const UNIlower = [97,122];
+const UNIsym = [33,47];
+const clipboard = document.getElementById('clipboard');
+
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = results.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
+
+
+document.querySelector("#generate").addEventListener('click', ()=> {
+  const length = document.querySelector("#length").value;
+  const upper = document.querySelector("#uppercase").checked;
+  const lower = document.querySelector("#lowercase").checked;
+  const numbers = document.querySelector("#numbers").checked;
+  const symbols = document.querySelector("#symbols").checked;
+  
+  const selector = [];
+  const password = [];
+  //String.fromCharCode();
+  if(upper===true){
+    for(let i=UNIupper[0]; i<= UNIupper[1]; i++){
+      selector.push(i);
+    }
+  }
+  if(numbers===true){
+    for(let i=UNInum[0]; i<= UNInum[1]; i++){
+      selector.push(i);
+    }
+  }
+  if(symbols===true){
+    for(let i=UNIsym[0]; i<= UNIsym[1]; i++){
+      selector.push(i);
+    }
+  }
+  if(lower===true){
+    for(let i=UNIlower[0]; i<= UNIlower[1]; i++){
+      selector.push(i);
+    }
+  }
+  
+  for(let i = 0; i< length; i++){
+    password.push(String.fromCharCode(selector[Math.floor(Math.random()*selector.length)]))
+  }
+  results.textContent = password.join("");
+})
+
+function showGenerate() {
+  var x = document.getElementById("generateDiv");
+  $('#generateDiv').toggle(1000);
+}
